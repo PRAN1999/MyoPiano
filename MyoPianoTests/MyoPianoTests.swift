716b42c3127902f58e160d55f9ceaeac2847ea4f
Pranay Neelagiri <pneeldam@gmail.com>
2018-04-07 14:01:30 -0400

    Initial Commit

---
+++ MyoPianoTests/MyoPianoTests.swift
@@ -0,0 +1,36 @@
+//
+//  MyoPianoTests.swift
+//  MyoPianoTests
+//
+//  Created by Pranay Neelagiri on 4/7/18.
+//  Copyright © 2018 Pranay Neelagiri. All rights reserved.
+//
+
+import XCTest
+@testable import MyoPiano
+
+class MyoPianoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
